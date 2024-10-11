2bf950c72c658d0d0a81f55097f5d64692738ab6
Luis Locon <luis.locon@gmail.com>
2024-10-11 03:57:05 -0600

    removing unused const

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,1 @@
-export const BASE_URI_TESTNET =
-  import.meta.env.VITE_INFRA_SEPOLIA_URI + import.meta.env.VITE_INFRA_API_KEY;
-
 export const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS;
